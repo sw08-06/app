d6bf3897e723e084b07831bd0da89da1a8afaee1
lasse2507a <lran18@student.aau.dk>
2024-05-11 20:13:47 +0200

    design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,15 +19,19 @@
 
 <title>Harmony</title>
 <body class="bg-neutral-900 h-screen flex flex-col items-center">
-	<header class="text-neutral-300 text-2xl font-bold mt-4 mb-4 shadow-lg">Harmony</header>
-	<section class="bg-neutral-800 flex flex-col items-center rounded-lg shadow-lg p-4 mb-4">
+	<header class="text-neutral-300 text-2xl font-bold mt-3 mb-3 shadow-lg">Harmony</header>
+	<section
+		class="bg-neutral-800 flex flex-col items-center rounded-lg shadow-lg p-4 mb-2 border border-slate-900"
+	>
 		<div class="flex justify-between w-full">
 			<h2 class="text-lg text-neutral-300 mb-4">Four-Week Stress Heatmap</h2>
 			<Tooltip headerText={'Heatmap of Monthly Stress'} text={heatmapTooltipText} />
 		</div>
 		<Heatmap />
 	</section>
-	<section class="bg-neutral-800 flex flex-col items-center rounded-lg shadow-lg p-4">
+	<section
+		class="bg-neutral-800 flex flex-col items-center rounded-lg shadow-lg p-4 border border-slate-900"
+	>
 		<div class="flex justify-between w-full">
 			<h2 class="text-lg text-neutral-300 mb-4">Daily Stress Distribution of [date]</h2>
 			<Tooltip headerText={'Donut Chart of Daily Stress'} text={donutTooltipText} />
